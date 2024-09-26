347db7521b8b223cafeb8f0f8d7cb636c83b3595
culley <harrelson@gmail.com>
2024-09-26 15:33:09 -0700

    temp file cleanup

--- models/test.sql
+++ models/test.sql
@@ -1,5 +1,1 @@
-{{ config(materialized='table') }}
-
-SELECT 
-    {{ star_column_group(from=ref('stg_region_home_values_raw'), dates=False) }}
-FROM {{ ref('stg_region_home_values_raw') }}
+-- {{ config(materialized='table') }}

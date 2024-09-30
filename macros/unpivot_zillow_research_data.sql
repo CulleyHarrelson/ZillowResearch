a9a5edd25881605680a04d71db1e2b02c6ea9ddd
culley <harrelson@gmail.com>
2024-09-30 15:00:05 -0700

    updating macro

--- macros/unpivot_zillow_research_data.sql
+++ macros/unpivot_zillow_research_data.sql
@@ -1,8 +1,11 @@
 {% macro unpivot_zillow_research_data(table_name) %}
 
 {%- set source_table = source('zillow_research', table_name) -%}
-{%- set columns = run_query("SELECT * FROM " ~ source_table ~ " LIMIT 0") -%}
-{%- set column_names = columns.columns | reject("in", ["regionid","sizerank","regionname","regiontype","statename","state","metro","countyname","city","statecodefips","municipalcodefips","id"]) | list -%}
+{%- set column_names = dbt_utils.get_filtered_columns_in_relation(
+    from=source_table,
+    except=["regionid","sizerank","regionname","regiontype","statename","state","metro","countyname","city","statecodefips","municipalcodefips","id"]
+) -%}
+
 
 WITH stg_data AS (
     SELECT DISTINCT

75461e8b2c0436bbceec3ea971279b4c45e10a21
culley <harrelson@gmail.com>
2024-09-30 14:44:25 -0700

    updating the macro for athena and trino sql

--- macros/unpivot_zillow_research_data.sql
+++ macros/unpivot_zillow_research_data.sql
@@ -1,35 +1,49 @@
 {% macro unpivot_zillow_research_data(table_name) %}
 
 {%- set source_table = source('zillow_research', table_name) -%}
-{%- set columns = adapter.get_columns_in_relation(source_table) -%}
-{%- set column_names = dbt_utils.get_filtered_columns_in_relation(
-    from=source_table,
-    except=["RegionID", "SizeRank", "RegionName", "RegionType", "StateName"]
-) -%}
+{%- set columns = run_query("SELECT * FROM " ~ source_table ~ " LIMIT 0") -%}
+{%- set column_names = columns.columns | reject("in", [,"regionid","sizerank","regionname","regiontype","statename","state","metro","countyname","city","statecodefips","municipalcodefips","id"]) | list -%}
 
 WITH stg_data AS (
     SELECT DISTINCT
-        "RegionID" as region_id,
-        "SizeRank" as size_rank,
-        "RegionName" as region_name,
-        "RegionType" as region_type,
-        "StateName" as state_name,
-        {{ dbt_utils.star(from=source_table, except=["RegionID", "SizeRank", "RegionName", "RegionType", "StateName"]) }}
+        `regionid`,
+        `sizerank`,
+        `regionname`,
+        `regiontype`,
+        `statename`,
+        `state`,
+        `metro`,
+        `countyname`,
+        `city`,
+        `statecodefips`,
+        `municipalcodefips`,
+        `id`,
+        {% for column in column_names %}
+            `{{ column }}` as `{{ column }}`{% if not loop.last %},{% endif %}
+        {% endfor %}
     FROM {{ source_table }}
 )
 
 {% for column in column_names %}
     select
-        region_id,
-        size_rank,
-        region_name,
-        region_type,
-        state_name,
+        `regionid`,
+        `sizerank`,
+        `regionname`,
+        `regiontype`,
+        `statename`,
+        `state`,
+        `metro`,
+        `countyname`,
+        `city`,
+        `statecodefips`,
+        `municipalcodefips`,
+        `id`,
         '{{ column }}' as metric_date,
-        "{{ column }}" as metric_value
+        `{{ column }}` as metric_value
     from stg_data
     {% if not loop.last %}
     UNION ALL
     {% endif %}
 {% endfor %}
 {% endmacro %}
+

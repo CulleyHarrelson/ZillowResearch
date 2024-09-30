02236f4655361a3302e31974778d15335fae366d
culley <harrelson@gmail.com>
2024-09-30 14:48:49 -0700

    syntax repair

--- macros/unpivot_zillow_research_data.sql
+++ macros/unpivot_zillow_research_data.sql
@@ -2,7 +2,7 @@
 
 {%- set source_table = source('zillow_research', table_name) -%}
 {%- set columns = run_query("SELECT * FROM " ~ source_table ~ " LIMIT 0") -%}
-{%- set column_names = columns.columns | reject("in", [,"regionid","sizerank","regionname","regiontype","statename","state","metro","countyname","city","statecodefips","municipalcodefips","id"]) | list -%}
+{%- set column_names = columns.columns | reject("in", ["regionid","sizerank","regionname","regiontype","statename","state","metro","countyname","city","statecodefips","municipalcodefips","id"]) | list -%}
 
 WITH stg_data AS (
     SELECT DISTINCT

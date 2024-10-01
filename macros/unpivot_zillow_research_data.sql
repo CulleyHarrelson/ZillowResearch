9c3239e841bb79bf4c7bd3d525e2320cda620901
culley <harrelson@gmail.com>
2024-09-30 18:14:08 -0700

    removing duplicate columns

--- macros/unpivot_zillow_research_data.sql
+++ macros/unpivot_zillow_research_data.sql
@@ -3,7 +3,7 @@
 {%- set source_table = source('zillow_research', table_name) -%}
 {%- set column_names = dbt_utils.get_filtered_columns_in_relation(
     from=source_table,
-    except=["regionid","sizerank","regionname","regiontype","statename","state","metro","countyname","city","statecodefips","municipalcodefips"]
+    except=["regionid","sizerank","regionname","regiontype","statename","state","metro","countyname","city","statecodefips","municipalcodefips", "regionid_2","sizerank_2","regionname_2","regiontype_2","statename_2","state_2","metro_2","countyname_2","city_2","statecodefips_2","municipalcodefips_2"]
 ) -%}
 
 

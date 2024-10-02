46f09062d57f7abfac71dd8a7b4eeaab616aa70f
culley <harrelson@gmail.com>
2024-10-02 07:07:14 -0700

    adding trino functions

--- models/marts/regions/regional_home_value_dates.sql
+++ models/marts/regions/regional_home_value_dates.sql
@@ -2,24 +2,24 @@
 
 WITH date_spine AS (
     SELECT DISTINCT home_value_date AS month_end_date
-    FROM {{ ref('int_regions_home_values_cleaned') }}
+    FROM {{ ref('home_values') }}
 )
 SELECT
     month_end_date,
     DATE_TRUNC('month', month_end_date) AS month_start_date,
-    TO_CHAR(month_end_date, 'YYYY-MM') AS month_key,
-    EXTRACT(YEAR FROM month_end_date) AS year,
-    EXTRACT(MONTH FROM month_end_date) AS month,
-    TO_CHAR(month_end_date, 'Month') AS month_name,
-    TO_CHAR(month_end_date, 'Mon') AS month_short_name,
-    EXTRACT(QUARTER FROM month_end_date) AS quarter,
+    format_datetime(month_end_date, '%Y-%m') AS month_key,
+    CAST(year(month_end_date) AS INTEGER) AS year_number,
+    CAST(month(month_end_date) AS INTEGER) AS month_number,
+    format_datetime(month_end_date, '%B') AS month_full_name,
+    format_datetime(month_end_date, '%b') AS month_short_name,
+    CAST(quarter(month_end_date) AS INTEGER) AS quarter_number,
     DATE_TRUNC('quarter', month_end_date) AS quarter_start_date,
     DATE_TRUNC('year', month_end_date) AS year_start_date,
     CASE 
-        WHEN EXTRACT(MONTH FROM month_end_date) IN (12, 1, 2) THEN 'Winter'
-        WHEN EXTRACT(MONTH FROM month_end_date) IN (3, 4, 5) THEN 'Spring'
-        WHEN EXTRACT(MONTH FROM month_end_date) IN (6, 7, 8) THEN 'Summer'
+        WHEN month(month_end_date) IN (12, 1, 2) THEN 'Winter'
+        WHEN month(month_end_date) IN (3, 4, 5) THEN 'Spring'
+        WHEN month(month_end_date) IN (6, 7, 8) THEN 'Summer'
         ELSE 'Fall'
-    END AS season,
-    EXTRACT(YEAR FROM month_end_date) * 100 + EXTRACT(MONTH FROM month_end_date) AS year_month_int
+    END AS season_name,
+    CAST(year(month_end_date) * 100 + month(month_end_date) AS INTEGER) AS year_month_int
 FROM date_spine

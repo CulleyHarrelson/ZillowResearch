7a3937db2ae91447700e6ada49069cda1d30b794
culley <harrelson@gmail.com>
2024-10-02 06:55:12 -0700

    fixing invalid name

--- models/intermediate/regions/int_regions_home_values_cleaned.sql
+++ models/intermediate/regions/int_regions_home_values_cleaned.sql
@@ -4,5 +4,8 @@ SELECT DISTINCT
     regionid AS region_id,
     metric_date AS home_value_date,
     metric_value AS home_value
-FROM {{ source('zillow_research', 'raw_regions_home_values') }}
+FROM {{ source('zillow_research', 'raw_home_values') }}
 WHERE metro not like '"%'
+
+
+
